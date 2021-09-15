bd8857b50e64071a6ee3b0b64a898e7da6b60100
Gavyn Riebau <gavyn.riebau@gmail.com>
2021-09-15 16:11:15 +0800

    Lighten background when diffing

--- lua/colors.lua
+++ lua/colors.lua
@@ -17,7 +17,7 @@ colorscheme onebuddy
 highlight DiffAdd cterm=NONE ctermfg=0 ctermbg=2 gui=NONE guifg=NONE guibg=#004000
 highlight DiffDelete cterm=NONE ctermfg=0 ctermbg=1 gui=NONE guifg=NONE guibg=#300000
 highlight DiffChange cterm=NONE ctermfg=NONE ctermbg=NONE gui=NONE guifg=NONE guibg=NONE
-highlight DiffText cterm=NONE ctermfg=0 ctermbg=6 gui=NONE guifg=0 guibg=#000080
+highlight DiffText cterm=NONE ctermfg=0 ctermbg=6 gui=NONE guifg=0 guibg=#000070
 
 " Hop jump word colors
 highlight HopNextKey guifg=#ff0000 gui=bold
