08d103e12a281640f695244741730d3566606ba0
Gavyn Riebau <gavyn.riebau@gmail.com>
2021-09-27 11:26:33 +0800

    Map <leader>n to toggle tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,8 +4,7 @@ local o = { noremap = true, silent = true }
 local m = vim.api.nvim_set_keymap
 
 m('n', '<Leader>ve', ':tabe ~/.config/nvim/init.lua<CR>', o)
-m('n', '<Leader>nn', ':NvimTreeToggle<CR>', o)
-m('n', '<Leader>nf', ':NvimTreeFindFile<CR>', o)
+m('n', '<Leader>n', ':NvimTreeToggle<CR>', o)
 m('n', '<Leader>x', ':HopWord<CR>', o)
 m('n', '<Leader>tt', ':Telescope<CR>', o)
 m('n', '<Leader>tf', ':Telescope fd<CR>', o)
