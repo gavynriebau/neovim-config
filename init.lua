2f6e8187134df6f2a86db192d4d3b66a02c6584f
Gavyn Riebau <gavyn.riebau@gmail.com>
2023-10-25 11:45:57 +0800

    Setup autocomplete

--- init.lua
+++ init.lua
@@ -36,6 +36,7 @@ vim.cmd.colorscheme('nightfox')
 require('hop').setup()
 require('lsp')
 require('nvim-tree').setup({
+  sync_root_with_cwd = true,
   tab = {
     sync = {
       open = true -- Re-open explorer if new tab opened
@@ -65,6 +66,40 @@ require('telescope').setup {
 }
 
 vim.api.nvim_create_user_command('ClearQF', utils.clear_quickfix, {})
+  
+local cmp = require'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    --{ name = 'vsnip' }, -- For vsnip users.
+    { name = 'luasnip' }, -- For luasnip users.
+    -- { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
+})
 
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- plugin manager
@@ -72,6 +107,7 @@ return require('packer').startup(function(use)
   use 'nvim-tree/nvim-tree.lua'
   use 'EdenEast/nightfox.nvim'
   use 'karb94/neoscroll.nvim'
+  use {'ojroques/nvim-bufdel'}
 
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
@@ -89,6 +125,15 @@ return require('packer').startup(function(use)
       require('lsp')
     end
   }
+
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  use 'L3MON4D3/LuaSnip'
+
 
   local builtin = require('telescope.builtin')
   vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
